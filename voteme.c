a0aec598403d1e6735d1cc4a0f519e79bdac2d33
someonebutnotme <chathursha@protonmail.com>
2025-09-05 18:22:41 +0530

    Removed fflush(stdin)

--- voteme.c
+++ voteme.c
@@ -94,7 +94,9 @@ void showmainmenu(void){
 				printf("\n%s[%s-%s] %sUnknown Option!%s\n", RESET_COLORS, RED_ON_BLACK, RESET_COLORS, RED_ON_BLACK, RESET_COLORS);
 				continue;
 		}
+		return;
 	}
+	return;
 }
 
 int main(void){
